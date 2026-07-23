e6be19814eab9db93e8ca6a054a52d9e545ff137
bpriyadharshinicse28-glitch <b.priyadharshini.cse28@princedrkvasudevan.com>
2026-07-23 10:21:01 +0530

    Create MainLayout.js

---
+++ src/templates/MainLayout.js
@@ -0,0 +1,17 @@
+import Header from "../Organisms/Header";
+import ThemePanel from "../Organisms/ThemePanel";
+import Content from "../Organisms/Content";
+import Footer from "../Organisms/Footer";
+
+function MainLayout() {
+  return (
+    <>
+      <Header />
+      <ThemePanel />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default MainLayout;
